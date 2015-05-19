3889a0850c5d73de7b5970f4c0b40fcb8d8a3cd3
Arun Singh <arunjeet.singh@live.com>
2015-05-19 02:52:25 -0700

    Update weblink.js

--- weblink.js
+++ weblink.js
@@ -2,7 +2,6 @@
 
 (function () {
     "use strict";
-    if(WinJS) {
     var page = WinJS.UI.Pages.define("/index.html", {
         ready: function (element, options) {
             var dataTransferManager = Windows.ApplicationModel.DataTransfer.DataTransferManager.getForCurrentView();
@@ -13,7 +12,7 @@
             var dataTransferManager = Windows.ApplicationModel.DataTransfer.DataTransferManager.getForCurrentView();
             dataTransferManager.removeEventListener("datarequested", dataRequested);
         }
-    }});
+    });
 
     function dataRequested(e) {
         var request = e.request;
